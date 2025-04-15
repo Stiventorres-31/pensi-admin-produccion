d2749b5f9434122323077826cd896dded527f5d5
Web samir <websamir2021@gmail.com>
2025-04-15 15:25:27 -0500

    listo para produccion

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-
+import storage from '../storage/storage';
 
 const axiosInstance = axios.create({
    // baseURL: 'http://127.0.0.1:8000',
-   baseURL: 'https://pensi-api-production-production.up.railway.app',
+   baseURL: storage.getAPI_URL(),
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
