467ef82617c5d013cb552a5df998f9886af0ff19
Web samir <websamir2021@gmail.com>
2025-04-15 14:30:38 -0500

    Se cambio el endpoint a produccion

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-   // baseURL: 'https://pensi-api-production.up.railway.app',
+   // baseURL: 'http://127.0.0.1:8000',
+   baseURL: 'pensi-api-production-production.up.railway.app',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
